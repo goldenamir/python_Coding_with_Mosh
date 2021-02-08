55a817ca879d3e6704b7630f7e4557ef7e6515e2
amir <emir.taghizadeh@gmail.com>
2021-02-08 12:10:28 +0100

    adding accessig items in list

--- List.py
+++ List.py
@@ -1,3 +1,15 @@
+""" Accessing Items """
+
 letters = ["a", "b", "c", "d"]
 letters[0] = 'A'
 print(letters[0])
+print(letters[-1])
+print(letters[0:3])
+print(letters[:3])
+print(letters[:])  # it takes whole elements
+print(letters[::2])  # takes each two element
+
+numbers = list(range(20))
+print(numbers[:])
+print(numbers[::2])
+print(numbers[::-1])  # it counts from end to start
