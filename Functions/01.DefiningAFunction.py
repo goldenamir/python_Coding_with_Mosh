4a3b361190a5ed281b950e1d7466fed1f732aa92
Amir <amir.taghizadeh@eposture.se>
2021-04-11 14:21:22 +0200

    adding function

---
+++ Functions/01.DefiningAFunction.py
@@ -0,0 +1,10 @@
+# naming a function should be all in LOWER CASE
+
+# after a function and identation we have to put 2 lines empty
+
+def greet():
+    print('hi ther')
+    print('welcome aboard')
+
+
+greet() # calling the function
