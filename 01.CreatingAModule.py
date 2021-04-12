ca3275c0f72408c73798b34fab9b4c192a12a36d
Amir <amir.taghizadeh@eposture.se>
2021-04-12 22:22:50 +0200

    adding first session of modules

---
+++ 01.CreatingAModule.py
@@ -0,0 +1,21 @@
+'''
+The major aim of MODULE is putting related OBJECT in the same file.python
+e.g., in a supermarket we are going to find fruits in the related section
+you can not find washing materials in the fruits section. 
+'''
+
+# For writing a module we are using SMALL LETTERS for NAMING
+# If the name of the module has more than one name, we use UNDERSCORE
+
+
+def calc_tax():
+    pass
+
+
+def calc_shipping():
+    pass
+
+
+# for using this module we are going to use the below code 
+
+# from 01.CreatingAModule import (ctrl+space  'for seeing all the functions in the module')
---
+++ Modules/exmaples/app.py
@@ -0,0 +1,9 @@
+from sales import calc_shipping, calc_tax # now what we are calling are the functions
+import sales # now what we call after sales.method is called METHOD
+
+sales.calc_shipping()
+sale.calc_tax()
+
+
+calc_shipping()
+calc_tax()
